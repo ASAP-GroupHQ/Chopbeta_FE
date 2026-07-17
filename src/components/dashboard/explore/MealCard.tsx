0be9842e916fd6b61fa8845e52f2f0429193bd97
Gideon ugwu <gideonchib234@gmail.com>
2026-07-17 14:02:09 +0100

    changes

--- src/components/dashboard/explore/MealCard.tsx
+++ src/components/dashboard/explore/MealCard.tsx
@@ -15,7 +15,7 @@ export default function MealCard({ title, desc, price, calories, imageUrl }: Pro
   return (
     <div className="bg-white rounded-2xl overflow-hidden shadow-sm border border-gray-100 flex flex-col group hover:shadow-md transition-shadow">
       <div className="relative h-44 w-full bg-gray-200 overflow-hidden">
-        <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent z-10" />
+        <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent z-10" />
         <img
           src={imageUrl || "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
           alt={title}
